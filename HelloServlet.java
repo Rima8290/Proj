640900f355184e7363f4ad1f92143fd2423ff2cf
Rima8290 <51225627+Rima8290@users.noreply.github.com>
2019-06-21 05:38:26 -0700

    JSP_Servlet_Rest

---
+++ HelloServlet.java
@@ -0,0 +1,30 @@
+package com.lti.webServlet.jsp;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//@WebServlet(urlPatterns = "/HelloServlet",loadOnStartUp=1)
+public class HelloServlet extends HttpServlet {
+	@Override
+	public void init() throws ServletException {
+		System.out.println("init called...");
+	}
+
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+			System.out.println("doGet called.....");
+		
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	@Override
+	public void destroy() {
+		System.out.println("destroy called...");
+	}
+	
+}
